634da146416818db1d7202472e1a88122ae66c7a
void6425 <alex@sivitermail.com>
2016-06-28 21:38:26 -0400

    Revert "Fixed Copper tools"
    This reverts commit 2acb813189d83578c779bafaaeff99cd6c7f13de.

--- main/java/jordan/bettercraft/init/BetterToolMaterials.java
+++ main/java/jordan/bettercraft/init/BetterToolMaterials.java
@@ -8,5 +8,5 @@ public class BetterToolMaterials
 	public static ToolMaterial RUBY = EnumHelper.addToolMaterial("RUBY", 2, 1000, 7.0F, 2.5F, 12);
 	public static ToolMaterial SAPPHIRE = EnumHelper.addToolMaterial("SAPPHIRE", 2, 1000, 7.0F, 2.5F, 12);
 	public static ToolMaterial EMERALD = EnumHelper.addToolMaterial("EMERALD", 2, 800, 13.0F, 3.5F, 8);
-	public static ToolMaterial COPPER = EnumHelper.addToolMaterial("COPPER", 2, 300, 6.0F, 2.0F, 10);
+	public static ToolMaterial COPPER = EnumHelper.addToolMaterial("COPPER", 2, 300, 6.0F, 2000.0F, 10);
 }
