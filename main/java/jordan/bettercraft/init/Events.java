f05e8031c4bd697e5ea41fbf662b92476c4334f3
void6425 <alex@sivitermail.com>
2016-06-29 22:52:28 -0400

    stuff

--- main/java/jordan/bettercraft/init/Events.java
+++ main/java/jordan/bettercraft/init/Events.java
@@ -12,7 +12,7 @@ public class Events {
 		if(event.getItemStack() != null && event.getWorld().getBlockState(event.getPos()).getBlock() != null){
 			if(event.getWorld().getBlockState(event.getPos()).getBlock().equals(BetterBlocks.ALTAR)
 					&& event.getItemStack().getItem().equals(Items.SUGAR ) && event.getEntityPlayer().inventory.hasItemStack(new ItemStack(BetterItems.TOME))){
-				
+				event.getEntityPlayer().inventory.getItemStack().setItemDamage(1);
 				event.getEntityPlayer().inventory.clearMatchingItems(Items.SUGAR, 0, 1, null);
 				event.getEntityPlayer().inventory.addItemStackToInventory(new ItemStack(BetterItems.SWIFT_ITEM));
 			}
