b3798355532d3f559bcd7ec5c0cf27728126817c
void6425 <alex@sivitermail.com>
2016-06-30 22:04:48 -0400

    merge fixes

--- main/java/jordan/bettercraft/init/BetterItems.java
+++ main/java/jordan/bettercraft/init/BetterItems.java
@@ -20,6 +20,7 @@ public class BetterItems
 	public static Item SWIFT_ITEM;
 	public static Item SALT;
 	public static Item TOME;
+	public static Item CATALYST;
 	
 	public static void init()
 	{
@@ -31,6 +32,7 @@ public class BetterItems
 		SWIFT_ITEM = registerItem(new ItemSwift(), "swift_item").setUnlocalizedName("swift_item").setCreativeTab(BetterTabs.tabBetterMiscellaneous);
 		TOME = registerItem(new ItemTome(), "tome_item").setUnlocalizedName("tome_item").setCreativeTab(BetterTabs.tabBetterMaterials);
 		SALT = registerItem(new Item(), "salt").setUnlocalizedName("salt").setCreativeTab(BetterTabs.tabBetterMaterials);
+		CATALYST = registerItem(new Item(), "catalyst").setUnlocalizedName("catalyst").setCreativeTab(BetterTabs.tabBetterMaterials);
 	}
 	public static void registerRenders()
 	{
@@ -42,6 +44,7 @@ public class BetterItems
 		registerRender(SWIFT_ITEM);
 		registerRender(SALT);
 		registerRender(TOME);
+		registerRender(CATALYST);
 	}
 	
 	public static void registerRender(Item item)
