ed25224e95f73e63744a1310ee43a99d7ec3abf3
void6425 <alex@sivitermail.com>
2016-07-02 23:15:24 -0400

    Added To-do file

--- main/java/jordan/bettercraft/init/BetterEvents.java
+++ main/java/jordan/bettercraft/init/BetterEvents.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumParticleTypes;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingJumpEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickBlock;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickItem;
@@ -16,7 +17,8 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
 
 public class BetterEvents {
-
+   
+	//start basic altar crafting method
 	@SubscribeEvent
 	public void speedCrafting(RightClickBlock event) {
 		if (event.getItemStack() != null && event.getWorld().getBlockState(event.getPos()).getBlock() != null) {
@@ -26,7 +28,7 @@ public class BetterEvents {
 				event.getEntityPlayer().inventory.clearMatchingItems(Items.SUGAR, 0, 1, null);
 				event.getEntityPlayer().inventory.clearMatchingItems(BetterItems.CATALYST, 0, 1, null);
 				event.getEntityPlayer().inventory.addItemStackToInventory(new ItemStack(BetterItems.SWIFT_ITEM));
-
+                event.getWorld().spawnParticle(EnumParticleTypes.LAVA, event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), 0, 0, 0, null);
 			}
 
 		}
@@ -44,7 +46,13 @@ public class BetterEvents {
 		}
 
 	}
-
+  //end the altar crafting
+	
+	
+	
+	
+	
+	
 	// this works but i can see it being really not server freindly
 	@SubscribeEvent
 	public void PlayerTick(PlayerTickEvent event){
@@ -53,6 +61,8 @@ public class BetterEvents {
 		}else{
 			event.player.capabilities.allowFlying = false;
 		}
+		
+			
 	}
 	
 	
